68d0059496f1eaee2ea41a9cb4644fedc6b265b5
Ling Kai <94221160+ratSpoT@users.noreply.github.com>
2025-08-28 21:39:03 +0800

    Update tempConversion.js
    did corrections for 11_tempConversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,11 @@
 const convertToCelsius = function(Fahrenheit) {
   let Celsius = (Fahrenheit - 32)* (5/9);
-  return Math.floor(Celsius * 10) / 10;
+  return Math.round(Celsius * 10) / 10;
 };
 
 const convertToFahrenheit = function(Celsius) {
   let Fahrenheit = Celsius * (9/5) + 32;
-  return Math.floor(Fahrenheit * 10) / 10;
+  return Math.round(Fahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
