91457f4d4db98097737cce85559a6b2b2339f823
Ling Kai <94221160+ratSpoT@users.noreply.github.com>
2025-09-08 20:40:45 +0800

    Update findTheOldest.js
    completed 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  let aAge = 0;
+  let bAge = 0;
+  return people.sort((a, b) => {
+    if (!("yearOfDeath" in a)) {
+      aAge = 2025 - a.yearOfBirth;
+    } else {
+      aAge = a.yearOfDeath - a.yearOfBirth;
+    }
+    if (!("yearOfDeath" in b)) {
+      bAge = 2025 - b.yearOfBirth;
+    } else {
+      bAge = b.yearOfDeath - b.yearOfBirth;
+    }
+    return bAge - aAge;
+  })[0];
 };
 
 // Do not edit below this line
