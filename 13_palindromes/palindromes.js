05f436bd47070117ab0a13d8198b69a0bb5436a3
Ling Kai <94221160+ratSpoT@users.noreply.github.com>
2025-09-08 17:41:03 +0800

    Update palindromes.js
    started on 13_palindrome

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+  let front = 0;
+  let back = word.length;
+  while (front < back) {
+    if (!(word[front] >= "a" && word[front] <= "z" || word[front] >= "A" && word[front] <= "Z")) {
+      front++;
 };
 
 // Do not edit below this line
