f3ccaeb95451ed8f70ba3563641ba1dd5782ca0e
Ling Kai <94221160+ratSpoT@users.noreply.github.com>
2025-09-08 19:52:17 +0800

    Update palindromes.js
    completed 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,10 +1,34 @@
 const palindromes = function (word) {
   let front = 0;
-  let back = word.length;
+  let back = word.length-1;
   while (front < back) {
-    if (!(word[front] >= "a" && word[front] <= "z" || word[front] >= "A" && word[front] <= "Z")) {
+    while (!isalpha(word[front])) {
+      if (front >= back){
+        return true;
+      }
       front++;
-};
+    };
 
+    while (!isalpha(word[back])) {
+      if (front >= back){
+        return true;
+      }
+      back--;
+    };
+
+    if (word[front].toLowerCase() !== word[back].toLowerCase()) {
+      return false;
+    }
+
+    front++;
+    back--;
+  }
+
+  return true;
+}
+
+function isalpha(char) {
+  return (char >= "a" && char <= "z" || char >= "A" && char <= "Z" || char >= "0" && char <= "9");
+}
 // Do not edit below this line
 module.exports = palindromes;
