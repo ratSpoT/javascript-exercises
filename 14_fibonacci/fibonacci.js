9ec32131a24c426356c7dbb4c815cf66e95eb795
Ling Kai <94221160+ratSpoT@users.noreply.github.com>
2025-09-08 20:07:59 +0800

    Update fibonacci.js
    completed 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+  if (num <= 0) {
+    return 0
+  }
+  
+  let a = 1;
+  let b = 1;
+  for (let i=3; i<=num; i++) {
+    let temp = b;
+    b += a;
+    a = temp;
+  }
+  return b;
+    
 };
 
 // Do not edit below this line
